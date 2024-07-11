bc948ba1bce9693289b1d1da0dbdd0ef38b6f030
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-07-11 16:52:18 +0530

    Create string2.cpp

---
+++ string2.cpp
@@ -0,0 +1,10 @@
+/*how to take string as input and then show it*/
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cout<<"enter the string "<<endl;
+    cin>>s;
+    cout<<"the string is "<<s;
+    return 0;
+}
