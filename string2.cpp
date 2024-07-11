f7ca7031ff700c27b0c08eef46c4d9a551015513
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-07-11 16:57:24 +0530

    Update string2.cpp

--- string2.cpp
+++ string2.cpp
@@ -8,3 +8,33 @@ int main(){
     cout<<"the string is "<<s;
     return 0;
 }
+/*Methid2*/
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cout<<"enter the string "<<endl;
+    getline(cin,s);//using getline() function
+    cout<<"the string is "<<s;
+    return 0;
+}
+/*Method 3*/
+#include <iostream>
+#include <sstream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+
+	string s = " GeeksforGeeks to the Moon ";
+	stringstream obj(s);
+	// string to store words individually
+	string temp;
+	// >> operator will read from the stringstream object
+	while (obj >> temp) {
+		cout << temp << endl;
+	}
+	return 0;
+}
