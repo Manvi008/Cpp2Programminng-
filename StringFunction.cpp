219b5c13b9b194e6843bdaf1f2f2ee3b094ca946
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-07-12 22:13:05 +0530

    Create StringFunction.cpp
    How to pass  string in a function

---
+++ StringFunction.cpp
@@ -0,0 +1,15 @@
+/* how to pass string in the function*/
+#include<iostream>
+using namespace std;
+   void Print_string(string s){
+    cout<<"the string is "<<s<<endl;
+    return;
+    }
+    int main(){
+        string s="ManviGarg";
+        Print_string(s);
+
+   return 0;
+}
+/*Output
+the String is ManviGarg*/
