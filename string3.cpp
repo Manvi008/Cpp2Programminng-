d3ba7f255d48a883661d885f89da9d16d844b807
Manvi Garg <91490442+Manvi008@users.noreply.github.com>
2024-07-12 21:48:15 +0530

    Create string3.cpp
    how to use getline() function

---
+++ string3.cpp
@@ -0,0 +1,15 @@
+/*to input the string using th e getline() function*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    string s;
+    cout<<"enter the string"<<endl;
+    getline(cin,s);
+    cout<<"the string is:"<<s<<endl;
+return 0;
+}
+/*Output:
+enter the string
+geeksfrogeeks
+the string isgeeksfrogeeks*/
